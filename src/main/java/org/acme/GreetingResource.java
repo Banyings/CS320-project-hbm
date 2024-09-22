9fc25c7b5e8ee3d87146ac08eb6b4660552a840b
Hyppolite Banyingela <banyingelahyppo@gmail.com>
2024-09-21 19:26:54 -0600

    created the frontend

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,7 +1,11 @@
 package org.acme;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -12,5 +16,45 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+
+    }
+
+    @Path("/personalizedHello/{name}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHello(@PathParam("name") String name) {
+        return "Hello " + name;
+
+    }
+
+    @POST
+    @Path("/personalized/HelloPost")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHelloPost(Person person) {
+        return "Hello, " + person.getFirstName() + " " + person.getLastName() + "!";
+    }
+
+    public static class Person {
+        @JsonProperty("firstName")
+        private String firstName;
+
+        @JsonProperty("lastName")
+        private String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
     }
 }
