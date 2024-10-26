e6b18b2874f26d73f2b4ecb2135f6f3d29629416
Hyppolite Banyingela <banyingelahyppo@gmail.com>
2024-10-25 18:50:58 -0600

    made some modifications in the home page

--- src/main/frontend/src/Navbar/Navbar.js
+++ src/main/frontend/src/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaImages, FaCalendarAlt, FaInfoCircle, FaEnvelope } from 'react-icons/fa';
+import { FaHome, FaImages, FaInfoCircle, FaEnvelope } from 'react-icons/fa';
+// import Image from'./Image.next';
 import './Navbar.css';
 
 export default function Navbar() {
@@ -13,7 +14,7 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <div className="logo">Logo</div>
-      
+      {/* <Image src="../../public/logo.png" alt="log-of-the"/> */}
       <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
         <span></span>
         <span></span>
@@ -23,9 +24,11 @@ export default function Navbar() {
       <div className={`nav-links ${isOpen ? 'show' : ''}`}>
         <Link to="/"><FaHome /> Home</Link>
         <Link to="/about"><FaInfoCircle /> About</Link>
-        <Link to="/booking"><FaCalendarAlt /> Booking</Link>
+        {/* <Link to="/booking"><FaCalendarAlt /> Booking</Link> */}
         <Link to="/contact"><FaEnvelope /> Contact</Link>
         <Link to="/services"><FaImages /> Services</Link>
+        {/* <Link to="/signup"><FaImages /> Signup</Link> */}
+
       </div>
     </nav>
   );
